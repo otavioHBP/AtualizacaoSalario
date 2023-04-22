50fa2dc7e851ce84aceb97695639858a6e77d5c1
Otavio <Otavio@DESKTOP-UOM8MPN>
2023-04-21 23:57:02 -0300

    Adicionar arquivos de projeto.

---
+++ AtualizacaoSalario/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtualizacaoSalario {
+        internal class Funcionario {
+        public String Name;
+        public Double SalarioBruto;
+        public Double Imposto;
+
+        public Double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentoSalarial(double porcentagem) {
+            SalarioBruto = (SalarioBruto + (SalarioBruto * porcentagem / 100));
+        }
+
+        public override String ToString() {
+            return Name
+                + ", Salário atualizado para R$ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ AtualizacaoSalario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace AtualizacaoSalario; 
+
+internal class Program {
+    static void Main(string[] args) {
+        
+        Funcionario func = new Funcionario();
+
+        Console.Write("Digite o nome do funcionário: ");
+        func.Name = Console.ReadLine();
+
+        Console.Write("Salário Bruto: R$");
+        func.SalarioBruto = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        Console.Write("Imposto atual: R$");
+        func.Imposto = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine($"Funcionário: {func}");
+
+        Console.WriteLine();
+        Console.Write("Qual porcentagem deseja aumentar o salário do funcionário? ");
+        double porcent = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        func.AumentoSalarial(porcent);
+
+        Console.WriteLine();
+        Console.WriteLine($"Dados atualizados do funcionário: {func}");
+
+        
+
+
+
+    }
+}
